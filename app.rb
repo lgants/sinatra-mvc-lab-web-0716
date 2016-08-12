4b4c0c2aced728bbbba228f1f0a11968c44a2c0c
lgants <lgants@gmail.com>
2016-08-12 09:54:47 -0400

    sinatra mvc lab complete -ltg

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    input = PigLatinizer.new
+    @p = input.to_pig_latin(params[:user_phrase])
+    erb :result
+  end
 end
